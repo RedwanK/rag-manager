bee2af9cdc4d789a68e9021dde0bc1a3b0a001d4
Redwan Kara <redwankara@gmail.com>
2025-12-01 15:02:52 +0100

    regenerate working migration

--- app/migrations/Version20251201140038.php
+++ app/migrations/Version20251201140038.php
@@ -7,20 +7,25 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20251128120000 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251201140038 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add soft delete support for document nodes';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE document_node ADD deleted_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE document_node DROP deleted_at');
     }
 }
